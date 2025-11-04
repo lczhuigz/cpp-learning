1f0ca2c3716c02303910af07fbedf6474a799309
lczhuigz <lczhuigz@163.com>
2025-11-04 11:35:31 +0800

    add chapter 5

---
+++ chapter 5/pointer.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int main(int argc, char **argv){
+    
+    int num = 10;
+    int * p1 = NULL, * p2 = NULL;
+    p1 = &num;
+    p2 = &num;
+
+    cout << "num = " << num << endl;
+
+    *p1 = 20;
+    cout << "num = " << num << endl;
+
+    *p2 = 30;
+    cout << "num = " << num << endl;
+
+
+    return 0;
+}
