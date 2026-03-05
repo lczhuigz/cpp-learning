e4e2214fa3025e1d430afc9a8c61d05809ebec9d
lczhuigz <lczhuigz@163.com>
2026-03-05 21:46:10 +0800

    2026-3-5

---
+++ chapter10/example4/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "time.hpp"
+
+
+int main(int argc, char **argv){
+
+    MyTime t1(1, 20);
+    int minutes = t1;           //implicit conversion
+    float f = float(t1);        //explicit conversion
+    std::cout << "minutes = " << minutes << std::endl;
+    std::cout << "minutes = " << f << std::endl;
+
+    MyTime t2 = 70;
+    std::cout << "t2 is " << t2 << std::endl;
+
+    MyTime t3;
+    t3 = 80;
+    std::cout << "t3 is " << t3 << std::endl;
+    
+
+    return 0;
+}
